1b7eb3a97cc193868b715bc858cceff25b340c17
Julio Gutierrez <a01282575@itesm.mx>
2019-10-18 16:14:46 -0500

    Added user model and create user feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  root 'landing#index'
 
  get 'catalog/index'
- get 'login/index'
+ get 'user/index'
  get 'cart/index'
- 
+
 end
